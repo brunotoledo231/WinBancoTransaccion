2f1a95d084a8a650d907c06b4bbe3fbe2f9a8911
brunotoledo231 <vrunotoledo@gmail.com>
2022-08-30 16:29:42 -0300

    subiendo nuevo ejercicio de banco

---
+++ Win_Banco/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Win_Banco
+{
+    partial class frmCuentas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.gpCliente = new System.Windows.Forms.GroupBox();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.lblDni = new System.Windows.Forms.Label();
+            this.txtDni = new System.Windows.Forms.TextBox();
+            this.gpCuentas = new System.Windows.Forms.GroupBox();
+            this.lblTipoCuenta = new System.Windows.Forms.Label();
+            this.lblSaldo = new System.Windows.Forms.Label();
+            this.txtSaldo = new System.Windows.Forms.TextBox();
+            this.lblCbu = new System.Windows.Forms.Label();
+            this.txtCbu = new System.Windows.Forms.TextBox();
+            this.cmbTipoCuenta = new System.Windows.Forms.ComboBox();
+            this.gpCliente.SuspendLayout();
+            this.gpCuentas.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(6, 28);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(67, 25);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(183, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // gpCliente
+            // 
+            this.gpCliente.Controls.Add(this.lblDni);
+            this.gpCliente.Controls.Add(this.txtDni);
+            this.gpCliente.Controls.Add(this.lblApellido);
+            this.gpCliente.Controls.Add(this.txtApellido);
+            this.gpCliente.Controls.Add(this.lblNombre);
+            this.gpCliente.Controls.Add(this.txtNombre);
+            this.gpCliente.Location = new System.Drawing.Point(12, 12);
+            this.gpCliente.Name = "gpCliente";
+            this.gpCliente.Size = new System.Drawing.Size(265, 116);
+            this.gpCliente.TabIndex = 2;
+            this.gpCliente.TabStop = false;
+            this.gpCliente.Text = "Clientes";
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(6, 54);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(44, 13);
+            this.lblApellido.TabIndex = 2;
+            this.lblApellido.Text = "Apellido";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(67, 51);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(183, 20);
+            this.txtApellido.TabIndex = 3;
+            // 
+            // lblDni
+            // 
+            this.lblDni.AutoSize = true;
+            this.lblDni.Location = new System.Drawing.Point(6, 80);
+            this.lblDni.Name = "lblDni";
+            this.lblDni.Size = new System.Drawing.Size(26, 13);
+            this.lblDni.TabIndex = 4;
+            this.lblDni.Text = "DNI";
+            // 
+            // txtDni
+            // 
+            this.txtDni.Location = new System.Drawing.Point(67, 77);
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(100, 20);
+            this.txtDni.TabIndex = 5;
+            // 
+            // gpCuentas
+            // 
+            this.gpCuentas.Controls.Add(this.cmbTipoCuenta);
+            this.gpCuentas.Controls.Add(this.lblTipoCuenta);
+            this.gpCuentas.Controls.Add(this.lblSaldo);
+            this.gpCuentas.Controls.Add(this.txtSaldo);
+            this.gpCuentas.Controls.Add(this.lblCbu);
+            this.gpCuentas.Controls.Add(this.txtCbu);
+            this.gpCuentas.Location = new System.Drawing.Point(283, 12);
+            this.gpCuentas.Name = "gpCuentas";
+            this.gpCuentas.Size = new System.Drawing.Size(348, 116);
+            this.gpCuentas.TabIndex = 6;
+            this.gpCuentas.TabStop = false;
+            this.gpCuentas.Text = "Cuentas";
+            this.gpCuentas.Enter += new System.EventHandler(this.gpCuentas_Enter);
+            // 
+            // lblTipoCuenta
+            // 
+            this.lblTipoCuenta.AutoSize = true;
+            this.lblTipoCuenta.Location = new System.Drawing.Point(6, 80);
+            this.lblTipoCuenta.Name = "lblTipoCuenta";
+            this.lblTipoCuenta.Size = new System.Drawing.Size(68, 13);
+            this.lblTipoCuenta.TabIndex = 4;
+            this.lblTipoCuenta.Text = "Tipo Cuenta:";
+            // 
+            // lblSaldo
+            // 
+            this.lblSaldo.AutoSize = true;
+            this.lblSaldo.Location = new System.Drawing.Point(6, 54);
+            this.lblSaldo.Name = "lblSaldo";
+            this.lblSaldo.Size = new System.Drawing.Size(37, 13);
+            this.lblSaldo.TabIndex = 2;
+            this.lblSaldo.Text = "Saldo:";
+            // 
+            // txtSaldo
+            // 
+            this.txtSaldo.Location = new System.Drawing.Point(67, 51);
+            this.txtSaldo.Name = "txtSaldo";
+            this.txtSaldo.Size = new System.Drawing.Size(137, 20);
+            this.txtSaldo.TabIndex = 3;
+            // 
+            // lblCbu
+            // 
+            this.lblCbu.AutoSize = true;
+            this.lblCbu.Location = new System.Drawing.Point(6, 28);
+            this.lblCbu.Name = "lblCbu";
+            this.lblCbu.Size = new System.Drawing.Size(32, 13);
+            this.lblCbu.TabIndex = 0;
+            this.lblCbu.Text = "CBU:";
+            // 
+            // txtCbu
+            // 
+            this.txtCbu.Location = new System.Drawing.Point(67, 25);
+            this.txtCbu.Name = "txtCbu";
+            this.txtCbu.Size = new System.Drawing.Size(275, 20);
+            this.txtCbu.TabIndex = 1;
+            // 
+            // cmbTipoCuenta
+            // 
+            this.cmbTipoCuenta.FormattingEnabled = true;
+            this.cmbTipoCuenta.Location = new System.Drawing.Point(77, 77);
+            this.cmbTipoCuenta.Name = "cmbTipoCuenta";
+            this.cmbTipoCuenta.Size = new System.Drawing.Size(265, 21);
+            this.cmbTipoCuenta.TabIndex = 7;
+            // 
+            // frmCuentas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gpCuentas);
+            this.Controls.Add(this.gpCliente);
+            this.Name = "frmCuentas";
+            this.Text = "CUENTAS";
+            this.gpCliente.ResumeLayout(false);
+            this.gpCliente.PerformLayout();
+            this.gpCuentas.ResumeLayout(false);
+            this.gpCuentas.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.GroupBox gpCliente;
+        private System.Windows.Forms.Label lblDni;
+        private System.Windows.Forms.TextBox txtDni;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.GroupBox gpCuentas;
+        private System.Windows.Forms.Label lblTipoCuenta;
+        private System.Windows.Forms.Label lblSaldo;
+        private System.Windows.Forms.TextBox txtSaldo;
+        private System.Windows.Forms.Label lblCbu;
+        private System.Windows.Forms.TextBox txtCbu;
+        private System.Windows.Forms.ComboBox cmbTipoCuenta;
+    }
+}
+
---
+++ Win_Banco/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win_Banco
+{
+    public partial class frmCuentas : Form
+    {
+        public frmCuentas()
+        {
+            InitializeComponent();
+        }
+
+        private void gpCuentas_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
